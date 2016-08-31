5c0e37aa8603ad8f22abc97fed37b339e93e5c56
Lorenzo Salvadori <lorenzo.salvadori.86@gmail.com>
2016-08-31 09:15:12 +0200

    Create parameter shortcut added

---
+++ Shortcuts/CreateParameter.js
@@ -0,0 +1,45 @@
+studio.menu.addMenuItem({
+    name: "Shortcuts\\CreateParameter",
+    keySequence: "Ctrl+Alt+P",
+    execute: function() {
+    	if(studio.window.browserCurrent() && studio.window.browserCurrent().isOfType("Event")){
+    		CreateParameter();
+    	}
+    	else {
+    		alert("Please select an event to create a Parameter");
+    	}
+    },
+});
+
+var CreateParameter = function()
+{
+	selectedEvent = studio.window.browserCurrent()
+	parameterContainer = selectedEvent.parameters;
+
+	var newParameter = studio.project.create("GameParameter");
+	newParameter.event = selectedEvent;
+	newParameter.minimum = 0;
+	newParameter.maximum = 1;
+	newParameter.name = SetDefaultName(newParameter, parameterContainer);
+};
+
+var SetDefaultName = function(objectInstance, objectContainer){
+	var i = 0;
+	var tempObjectNameInit = "New " + objectInstance.entity;
+	var tempObjectName;
+	var bNameIsValid = 0;
+	while (!bNameIsValid){
+		bNameIsValid = 1;
+		tempObjectName =  tempObjectNameInit + " (" + i + ")";
+		i++;
+		for (var e = 0; e < objectContainer.length; e++){
+			if (objectContainer[e].isOfType(objectInstance.entity) && objectContainer[e].name == tempObjectName){
+				bNameIsValid = 0;
+				break;
+			}
+		}
+	}
+
+	return tempObjectName;
+
+};
